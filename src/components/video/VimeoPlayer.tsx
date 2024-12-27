2b200135aa2f3fab0b9b389c608b216259073c6a
hvreynaldo <hvreynaldo@gmail.com>
2024-12-26 22:36:21 -0600

    Update VimeoPlayer.tsx

--- src/components/video/VimeoPlayer.tsx
+++ src/components/video/VimeoPlayer.tsx
@@ -7,11 +7,11 @@ const VimeoPlayer: FC = () => {
   return (
     <div style={{ padding: '56.25% 0 0 0', position: 'relative' }} className="rounded-lg overflow-hidden shadow-2xl">
       <iframe 
-        src="https://player.vimeo.com/video/1042277874?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+        src="https://player.vimeo.com/video/1042384751?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
         frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-        title="IF Ad 2.mp4"
+        title="IF Ad 4.mp4"
       />
     </div>
   );
